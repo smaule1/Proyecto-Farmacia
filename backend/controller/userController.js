839fb7b810557ce9c558c214861bcf4c80a2a9b4
smaule1 <samuelzv18@gmail.com>
2024-11-02 14:17:27 -0600

    funtcion createToken and bug fix

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -8,52 +8,55 @@ import bcrypt from "bcrypt";
 export const registrar = async (req, res) => {
     const reqBody = req.body;
 
-    
+
     const user = new User(reqBody);
 
-    try{    
+    try {
         await user.save();
-        const token = jwt.sign(
-            {
-                _id: user._id,
-                nombreUsuario: user.nombreUsuario,
-                rol: user.rol
-            },
-            process.env.JWT_PRIVATE_KEY,
-            { expiresIn: "15m"}
-        );          
-        res.cookie('userInfo', token, {httpOnly:true, maxAge: 60*60, sameSite:Strict}) //maxAge 1 hora
-        res.status(200).json({data: user._id});
-    }catch(error){                        
-        if(error.code === 11000){
+        const token = createToken(user);
+        res.cookie('userInfo', token, { httpOnly: true, maxAge: 60 * 60, sameSite: 'strict' }) //maxAge 1 hora
+        res.status(200).json({ data: { userId: user._id } });
+    } catch (error) {
+        if (error.code === 11000) {
             const message = `Ya existe un usario registrado con ese email`
-            res.status(400).json({success:false, message: message});        
-        }else{
-            res.status(400).json({success:false, message: error.message});        
+            res.status(400).json({ success: false, message: message });
+        } else {
+            res.status(400).json({ success: false, message: error.message });
         }
-        
+
     }
 };
 
 
-export const login = async (req, res) => {    
-    const {email, password} = req.body;
+export const login = async (req, res) => {
+    const { email, password } = req.body;
 
     //TODO: validation
-    
-    const user = await User.findOne({'email': email});    
-    if(!user){
-        res.status(400).json({success:false, message: 'El correo electónico no está registrado.'});
-        return;    
-    }    
-    const auth = await bcrypt.compare(password, user.password);    
-    if(!auth){
-        res.status(400).json({success:false, message: 'Contraseña incorrecta'});
+
+    const user = await User.findOne({ 'email': email });
+    if (!user) {
+        res.status(400).json({ success: false, message: 'El correo electónico no está registrado.' });
         return;
     }
-
-    res.status(200).json({data: user._id});
-
-
+    const auth = await bcrypt.compare(password, user.password);
+    if (!auth) {
+        res.status(400).json({ success: false, message: 'Contraseña incorrecta' });
+        return;
+    }
+    const token = createToken(user);
+    res.cookie('userInfo', token, { httpOnly: true, maxAge: 60 * 60, sameSite: 'strict' }) //maxAge 1 hora
+    res.status(200).json({ data: { userId: user._id } });
 
 };
+
+function createToken(user) {
+    const token = jwt.sign(
+        {
+            _id: user._id,
+            rol: user.rol
+        },
+        process.env.JWT_PRIVATE_KEY,
+        { expiresIn: "15m" }
+    );
+    return token;
+}
