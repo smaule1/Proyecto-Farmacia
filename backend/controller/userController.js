c85b84463edcef73f7b9e587e6a5226e7aec0d0b
smaule1 <samuelzv18@gmail.com>
2024-11-02 16:10:26 -0600

    Login with Token

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -15,7 +15,7 @@ export const registrar = async (req, res) => {
         await user.save();
         const token = createToken(user);
         res.cookie('userInfo', token, { httpOnly: true, maxAge: 60 * 60, sameSite: 'strict' }) //maxAge 1 hora
-        res.status(200).json({ data: { userId: user._id } });
+        res.status(200).json({ data: { user } });
     } catch (error) {
         if (error.code === 11000) {
             const message = `Ya existe un usario registrado con ese email`
@@ -29,23 +29,50 @@ export const registrar = async (req, res) => {
 
 
 export const login = async (req, res) => {
+
+    //Token Authenticaton
+
+    const cookies = req.cookies;
+    if (cookies) {
+        reqToken = cookies.userInfo;
+        if (reqToken){
+            jwt.verify(reqToken, process.env.JWT_PRIVATE_KEY, (err, decoded) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(decoded);
+                    //TODO: return user info                
+                }
+            })
+        }
+    }
+
+
+    //Credential Authentication
+
     const { email, password } = req.body;
 
-    //TODO: validation
-
+    //Validation
+    if (!email || !password) {
+        res.status(400).json({ message: 'Missing parameters in request body' });
+        return;
+    }
+    //Get User by Email
     const user = await User.findOne({ 'email': email });
     if (!user) {
         res.status(400).json({ success: false, message: 'El correo electónico no está registrado.' });
         return;
     }
+    //Check Password
     const auth = await bcrypt.compare(password, user.password);
     if (!auth) {
         res.status(400).json({ success: false, message: 'Contraseña incorrecta' });
         return;
     }
+    //Response Token and User data 
     const token = createToken(user);
     res.cookie('userInfo', token, { httpOnly: true, maxAge: 60 * 60, sameSite: 'strict' }) //maxAge 1 hora
-    res.status(200).json({ data: { userId: user._id } });
+    res.status(200).json({ data: { user } });
 
 };
 
