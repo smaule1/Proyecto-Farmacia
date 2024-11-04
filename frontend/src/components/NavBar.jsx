681ab5c6c3bcd655356a27919c53883821ddac41
smaule1 <samuelzv18@gmail.com>
2024-11-04 17:22:48 -0600

    Links en navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
 
 function NavBar({rol}) {
   const[name, setName] = useState('');
@@ -9,20 +10,20 @@ function NavBar({rol}) {
 
   function renderCorrectNavBar(){
     return(
-      <li className="nav-item" id="inicioSesion">
-        <a className="nav-link text-dark" href="/purchase">Registrar Compra</a>
+      <li className="nav-item">
+        <Link className="nav-link text-dark" to="/purchase">Registrar Compra</Link>
       </li>
     )
   }
   useEffect(() => {
     renderInput();
-  }, []);
+  });
 
   return (
     <header>
       <nav className="navbar navbar-expand-sm navbar-light bg-white border-bottom box-shadow mb-3">
         <div className="container">
-          <a className="navbar-brand h1 m-1" href="#">Sistema Beneficios</a>
+          <Link to='/temp' className="navbar-brand h1 m-1">Sistema Beneficios</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent"
             aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -30,11 +31,11 @@ function NavBar({rol}) {
           <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between">
             <ul className="navbar-nav flex-grow-1">
               <li className="nav-item" id="registro">
-                <a className="nav-link text-dark" href="#">Home</a>
+                <Link className="nav-link text-dark" to='/temp'>Home</Link>
               </li>
                 { rol === 'Usuario' && renderCorrectNavBar()}
               <li className="nav-item">
-                <a className="nav-link text-dark" href="/userHistory">{name}</a>
+                <Link className="nav-link text-dark" to="/userHistory">{name}</Link>
               </li>
             </ul>
           </div>
