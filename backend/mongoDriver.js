02cc1769f505bd14cc9986915cd3a6d90c26cb8b
RbnHrtd <ruben.is.hurtado05@gmail.com>
2025-06-01 16:19:39 -0600

    P9-UserModuleTest

--- backend/mongoDriver.js
+++ backend/mongoDriver.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 
 
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true }, dbName:'Farmacia' };
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true }, dbName:'FarmaciaTest' };
 
 export const connectDB = async () => {
     try {
      // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
     const conn = await mongoose.connect(process.env.MONGO_URI, clientOptions); 
-    mongoose.connection.useDb('Farmacia');
+    mongoose.connection.useDb('FarmaciaTest');
     console.log(`${conn.connection.host}`);
     } catch(error){
         console.error(`Error: ${error.message}`);
